20009ae78109c94c28123af0aaffd7ed1bedaa2a
Beatmeda <beatmeda@gmail.com>
2020-06-20 12:43:18 -0500

    Configuracion heroku.

--- utils/email.js
+++ utils/email.js
@@ -5,6 +5,7 @@ module.exports = {
         const result = await nodemailer.createTransport({
             host: process.env['HOST_EMAIL'],
             port: process.env['PORT_EMAIL'],
+            secure: true,
             auth: {
                 user: process.env['USER_EMAIL'],
                 pass: process.env['PASS_EMAIL']
